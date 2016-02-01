1b67cfce221f53b19a24b95b18365c27a2469bfa
Sazzad <sazzad.islam@dsinnovators.com>
2016-02-01 13:37:01 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+mvn exec:java   (to run the project)
---
+++ src/main/java/com/lynas/Main.java
@@ -0,0 +1,10 @@
+package com.lynas;
+
+/**
+ * Created by sazzad on 2/1/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
