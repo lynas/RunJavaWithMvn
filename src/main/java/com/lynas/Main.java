e8c19524df2c94c6a0f87840e482c262c472777d
Sazzad <sazzad.islam@dsinnovators.com>
2016-02-01 13:52:17 +0600

    running with some dependency

--- src/main/java/com/lynas/Main.java
+++ src/main/java/com/lynas/Main.java
@@ -1,10 +1,15 @@
 package com.lynas;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 /**
  * Created by sazzad on 2/1/16.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Student student = new Student(1,"sazzad");
+        Gson gson = new GsonBuilder().create();
+        System.out.println(gson.toJson(student));
     }
 }
---
+++ src/main/java/com/lynas/Student.java
@@ -0,0 +1,33 @@
+package com.lynas;
+
+/**
+ * Created by sazzad on 2/1/16.
+ */
+public class Student {
+    private int id;
+    private String name;
+
+    public Student() {
+    }
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
